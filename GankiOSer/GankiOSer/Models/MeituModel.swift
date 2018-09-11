2bc2aabf1bc20c0fc60189cbc49ac9259254a49d
木瓜 <lforme@163.com>
2018-09-11 16:54:38 +0800

    add meitu json model

---
+++ GankiOSer/GankiOSer/Models/MeituModel.swift
@@ -0,0 +1,17 @@
+//
+//  MeituModel.swift
+//  GankiOSer
+//
+//  Created by mugua on 2018/9/11.
+//  Copyright © 2018年 mugua. All rights reserved.
+//
+
+import Foundation
+import HandyJSON
+
+struct MeituModel: HandyJSON {
+    var createdAt: String!
+    var publishedAt: String!
+    var type: String!
+    var url: String!
+}
